637370672a38cfb76a9255f4681b2f154b239250
valereng <valeriengwu@gmail.com>
2023-02-21 09:41:54 -0800

    main.h

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 
 /**
   * _putchar - writes the character c stdout
-  * oc; The character to print
+  * @c: The character to print
   * Return: On success 1.
   * On error -1 is returned, and errno is set appropriately
   */
