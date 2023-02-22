09daad161ef0da4a6e1bb9c4df3f9846d2e75b57
valereng <valeriengwu@gmail.com>
2023-02-22 01:54:08 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 
  /**
-  * print_alphabet_x10&prints 10 times the alphabet in lower case
+   * print_alphabet_x10 -  prints 10 times the alphabet in lower case
   * followed by a new line
   */
 
@@ -28,7 +28,7 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 
 		  i++;
-         }
+	}
 
 
 }
