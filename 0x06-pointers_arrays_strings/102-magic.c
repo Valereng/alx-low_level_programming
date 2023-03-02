2e799545787d92e315f7ad61b7d30580830d2b9e
valereng <valeriengwu@gmail.com>
2023-03-02 12:27:12 -0800

    Done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main{void}
-{	
+int main(void)
+{
 int n;
 int a[5];
 int *p;
@@ -9,7 +9,6 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-   
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
