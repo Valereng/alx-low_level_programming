c69912b86621402d4d9b0a7aa3e2ec8b90b839de
valereng <valeriengwu@gmail.com>
2023-03-23 17:30:14 -0700

    fun

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef VAR_FUNC_H_
+#define VAR_FUNC_H_
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 	int _putchar(char c);
 	int sum_them_all(const unsigned int n, ...);
